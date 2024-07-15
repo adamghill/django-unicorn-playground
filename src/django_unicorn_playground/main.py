99e62e8c3d3f409f6a11e46a5ed63134d3f18b97
adamghill <adamghill@yahoo.com>
2024-07-14 22:28:29 -0400

    Split loading settings into a new file.

--- src/django_unicorn_playground/main.py
+++ src/django_unicorn_playground/main.py
@@ -1,15 +1,12 @@
 import os
 from pathlib import Path
-from typing import Any
-from uuid import uuid4
 
 from django import conf
 from django.core.management import execute_from_command_line
-from django_unicorn.components import UnicornView
 from typeguard import typechecked
 
-from django_unicorn_playground import urls
 from django_unicorn_playground.components import get_component_classes
+from django_unicorn_playground.settings import get_settings
 
 BASE_PATH = Path(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
@@ -19,70 +16,35 @@ class UnicornPlayground:
     def __init__(
         self,
         component_path: Path | str,
+        *,
         template_dir: str | None = None,
-        *args,  # noqa: ARG002
-        **kwargs,
+        **django_settings,
     ):
-        self.template_dir = template_dir
-        self.component_classes = get_component_classes(component_path)
+        """
+        Initialize a new playground.
+
+        Args:
+            component_path: Where the component lives on the filesystem.
+            template_dir: Used to specify a template directory.
+            django_settings: Override the default Django settings.
+        """
+
+        component_classes = get_component_classes(component_path)
 
         # Get default Django settings
-        self.settings = self._get_settings()
+        settings = get_settings(template_dir, component_classes)
 
-        # Override default settings with the UnicornPlayground kwargs
-        self.settings.update(**kwargs)
+        # Override default settings with any init kwargs
+        settings.update(**django_settings)
 
         # Set Django settings
-        conf.settings.configure(**self.settings)
-
-    def _get_components_setting(self) -> dict[str, type[UnicornView]]:
-        """Gets the `UNICORN.COMPONENTS` Django settings."""
-
-        components = {}
-
-        for component_class in self.component_classes:
-            component_name = component_class.__module__
-            components[component_name] = component_class
-
-        return components
-
-    def _get_templates_setting(self) -> list[dict[str, Any]]:
-        """Gets the `TEMPLATES` Django settings."""
-
-        template_dirs = []
-
-        if self.template_dir is not None:
-            template_dirs.append(self.template_dir)
-
-        templates = [
-            {
-                "BACKEND": "django.template.backends.django.DjangoTemplates",
-                "DIRS": template_dirs,
-                "APP_DIRS": True,
-            },
-        ]
-
-        return templates
-
-    def _get_settings(self) -> dict[str, Any]:
-        return {
-            "ALLOWED_HOSTS": "*",
-            "ROOT_URLCONF": urls,
-            "SECRET_KEY": str(uuid4()),
-            "DEBUG": True,
-            "TEMPLATES": self._get_templates_setting(),
-            "INSTALLED_APPS": (
-                "django.contrib.staticfiles",  # required for django-unicorn JavaScript
-                "django_unicorn",
-                "django_unicorn_playground",
-            ),
-            "UNICORN": {
-                "COMPONENTS": self._get_components_setting(),
-            },
-            "STATIC_URL": "static/",  # required for django-unicorn JavaScript
-        }
+        conf.settings.configure(**settings)
 
     def runserver(self, *, port: int = 8000):
-        """Starts the dev server."""
+        """Starts the dev server.
+
+        Args:
+            port: The port that the dev server should be run on.
+        """
 
         execute_from_command_line(["manage", "runserver", str(port)])
---
+++ src/django_unicorn_playground/settings.py
@@ -0,0 +1,60 @@
+from typing import TYPE_CHECKING, Any
+from uuid import uuid4
+
+from django_unicorn.components import UnicornView
+from typeguard import typechecked
+
+from django_unicorn_playground import urls
+
+
+@typechecked
+def _get_components_setting(component_classes: list[type[UnicornView]]) -> dict[str, type[UnicornView]]:
+    """Gets the `UNICORN.COMPONENTS` Django settings."""
+
+    components = {}
+
+    for component_class in component_classes:
+        component_name = component_class.__module__
+        components[component_name] = component_class
+
+    return components
+
+
+@typechecked
+def _get_templates_setting(template_dir: str | None) -> list[dict[str, Any]]:
+    """Gets the `TEMPLATES` Django settings."""
+
+    template_dirs = []
+
+    if template_dir:
+        template_dirs.append(template_dir)
+
+    templates = [
+        {
+            "BACKEND": "django.template.backends.django.DjangoTemplates",
+            "DIRS": template_dirs,
+            "APP_DIRS": True,
+        },
+    ]
+
+    return templates
+
+
+@typechecked
+def get_settings(template_dir: str | None, component_classes: list[type[UnicornView]]) -> dict[str, Any]:
+    return {
+        "ALLOWED_HOSTS": "*",
+        "ROOT_URLCONF": urls,
+        "SECRET_KEY": str(uuid4()),
+        "DEBUG": True,
+        "TEMPLATES": _get_templates_setting(template_dir),
+        "INSTALLED_APPS": (
+            "django.contrib.staticfiles",  # required for django-unicorn JavaScript
+            "django_unicorn",
+            "django_unicorn_playground",
+        ),
+        "UNICORN": {
+            "COMPONENTS": _get_components_setting(component_classes),
+        },
+        "STATIC_URL": "static/",  # required for django-unicorn JavaScript
+    }
